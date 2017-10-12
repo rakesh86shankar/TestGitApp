28cc3e745e258dc60454923c9363c216086d7936
rakesh86sankar <rakesh86shankar@gmail.com>
2017-10-12 08:51:58 +0530

    Log files added.

---
+++ app/src/main/java/com/news/rakeshsankar/testgitapp/TestActivity.java
@@ -0,0 +1,17 @@
+package com.news.rakeshsankar.testgitapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rakesh sankar on 10/12/2017.
+ */
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
